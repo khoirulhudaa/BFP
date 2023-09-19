1d1dc22935e6ca1300abe3bb5fefb6cdee4252ff
valclassdevelop <developervalclass.com>
2023-09-19 22:58:38 +0700

    project blackFire

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,100 +1,71 @@
 import React from "react";
 import "../assets/styles/footer.css";
+import { LogoBF } from "../assets/images";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
     return (
         <>
             <div className="footer-hdm">
                 <div className="footer-left-hdm">
-                    <img
-                        src="https://images-builder.vercel.app/img/logo_hdm.svg"
-                        alt="logo"
-                    />
-                    <div className="wrap-icon-hdm">
-                        <a href="">
-                            <div className="icons-hdm">
-                                <img
-                                    src="https://images-builder.vercel.app/img/ig_hdm.svg"
-                                    alt="icon"
-                                />
-                            </div>
-                        </a>
-                        <a href="">
-                            <div className="icons-hdm">
-                                <img
-                                    src="https://images-builder.vercel.app/img/twitter_hdm.svg"
-                                    alt="icon"
-                                />
-                            </div>
-                        </a>
-                        <a href="">
-                            <div className="icons-hdm">
-                                <img
-                                    src="https://images-builder.vercel.app/img/ig_hdm.svg"
-                                    alt="icon"
-                                />
-                            </div>
-                        </a>
-                        <a href="">
-                            <div className="icons-hdm">
-                                <img
-                                    src="https://images-builder.vercel.app/img/yt_hdm.svg"
-                                    alt="icon"
-                                />
-                            </div>
-                        </a>
-                    </div>
+                    <img src={LogoBF} alt="logo" className="logo" />
                 </div>
                 <div className="footer-right-hdm">
                     <div className="group-list-hdm">
                         <ul>
-                            <h3>SHORT LINKS</h3>
-                            <a href="">
-                                <li>home</li>
+                            <h3>HOMEPAGE</h3>
+                            <a href="#introduction">
+                                <li>Product</li>
                             </a>
-                            <a href="">
-                                <li>services</li>
+                            <a href="#function">
+                                <li>Function</li>
                             </a>
-                            <a href="">
-                                <li>screenshoot</li>
+                            <a href="#protection">
+                                <li>Protection</li>
                             </a>
-                            <a href="">
-                                <li>contact</li>
-                            </a>
-                        </ul>
-                    </div>
-                    <div className="group-list-hdm">
-                        <ul>
-                            <h3>HELP LINKS</h3>
-                            <a href="">
-                                <li>home</li>
-                            </a>
-                            <a href="">
-                                <li>services</li>
-                            </a>
-                            <a href="">
-                                <li>screenshoot</li>
-                            </a>
-                            <a href="">
-                                <li>contact</li>
+                            <a href="#type">
+                                <li>Type Firewall</li>
                             </a>
                         </ul>
                     </div>
                     <div className="group-list-hdm">
                         <ul>
-                            <h3>SHORT LINKS</h3>
-                            <a href="">
-                                <li>home</li>
-                            </a>
-                            <a href="">
-                                <li>services</li>
-                            </a>
-                            <a href="">
-                                <li>screenshoot</li>
-                            </a>
-                            <a href="">
-                                <li>contact</li>
-                            </a>
+                            <h3>TYPE FIREWALL</h3>
+                            <Link to={"/Proxy-firewall"}>
+                                <a>
+                                    <li>Proxy Firewall</li>
+                                </a>
+                            </Link>
+                            <Link to={"/Packet-filtering"}>
+                                <a>
+                                    <li>Packet Filtering</li>
+                                </a>
+                            </Link>
+                            <Link to={"/Next-generation"}>
+                                <a>
+                                    <li>Next Generation</li>
+                                </a>
+                            </Link>
+                        </ul>
+                    </div>
+                    <div className="group-list-hdm">
+                        <ul>
+                            <h3>ARTICLES</h3>
+                            <Link to={'https://www.niagahoster.co.id/blog/firewall-adalah/'}>
+                                <a>
+                                    <li>Firewall 1</li>
+                                </a>
+                            </Link>
+                            <Link to={'https://blog.myorbit.id/technology/fungsi-dan-cara-kerja-firewall/'}>
+                                <a>
+                                    <li>Firewall 2</li>
+                                </a>
+                            </Link>
+                            <Link to={'https://www.sekawanmedia.co.id/blog/firewall-adalah/'}>
+                                <a>
+                                    <li>Firewall 3</li>
+                                </a>
+                            </Link>
                         </ul>
                     </div>
                 </div>
